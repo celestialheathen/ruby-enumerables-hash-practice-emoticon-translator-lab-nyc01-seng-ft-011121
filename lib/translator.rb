36cb79aed20c1f7096faf8de9f2393206f25e156
Jingchao Yu <jingchaoyu@hotmail.com>
2021-01-06 23:12:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,19 +12,35 @@ end
 def get_japanese_emoticon(file, ame_emo)
     emoticons = load_library(file)
     emoticons.each { |name, values|
+<<<<<<< HEAD
         if values[:english] == ame_emo
             return values[:japanese]
         end
     }
     "Sorry, that emoticon was not found"
+=======
+        if values[0] == ame_emo
+            return values[1]
+        else 
+          puts "Sorry, not found."
+        end
+    }
+>>>>>>> 0e5f5c1291ebe0749a844b4e6d9b6f2403d823d4
 end
 
 def get_english_meaning(file, jap_emo)
     emoticons = load_library(file)
     emoticons.each { |name, values|
+<<<<<<< HEAD
         if values[:japanese] == jap_emo
             return name
         end
     }
     "Sorry, that emoticon was not found"
+=======
+        if values[1] == jap_emo
+            return name
+        end
+    }
+>>>>>>> 0e5f5c1291ebe0749a844b4e6d9b6f2403d823d4
 end
